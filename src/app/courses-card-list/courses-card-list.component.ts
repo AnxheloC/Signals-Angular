1232301121e3ba45390f30ed673c796442ba5ae5
AnxheloC <anxhelo.cuni@chweb.it>
2024-07-24 16:23:14 +0200

    Promise and signals course display and fetch

--- src/app/courses-card-list/courses-card-list.component.ts
+++ src/app/courses-card-list/courses-card-list.component.ts
@@ -2,6 +2,7 @@ import {Component, inject, input, output} from '@angular/core';
 import {RouterLink} from "@angular/router";
 import {Course} from "../models/course.model";
 import {MatDialog} from "@angular/material/dialog";
+import {openEditCourseDialog} from "../edit-course-dialog/edit-course-dialog.component";
 
 @Component({
   selector: 'courses-card-list',
@@ -13,5 +14,27 @@ import {MatDialog} from "@angular/material/dialog";
   styleUrl: './courses-card-list.component.scss'
 })
 export class CoursesCardListComponent {
+  dialog=inject(MatDialog);
+courses=input.required<Course[]>({
+  alias:"data",
+  // transform:  eshte nje pure function dhe i ben transform datave qe vijne per setter dhe getters dhe mund te mos kesh required inputs
+});
+coursesUpdated= output<Course>()
+courseDeleted=output<string>();
+//me ndryshimet qe bem tani na vjen si nje promise
+  async onEditCourse(course: Course) {
+   const newCourse = await openEditCourseDialog(
+     this.dialog,{
+       mode:'update',
+       title:'Update Existing Course',
+       course:course,
+     }
+   )
+    console.log(`course edited `, newCourse)
+    this.coursesUpdated.emit(newCourse)
+}
 
+  onCourseDelete(course: Course) {
+    this.courseDeleted.emit(course.id)
+  }
 }
--- src/app/services/courses.service.ts
+++ src/app/services/courses.service.ts
@@ -1,7 +1,7 @@
-import {Injectable} from "@angular/core";
+import {inject, Injectable} from "@angular/core";
 import { HttpClient } from "@angular/common/http";
 import {environment} from "../../environments/environment";
-import {firstValueFrom} from "rxjs";
+import {firstValueFrom, lastValueFrom} from "rxjs";
 import {Course} from "../models/course.model";
 import {GetCoursesResponse} from "../models/get-courses.response";
 
@@ -10,6 +10,31 @@ import {GetCoursesResponse} from "../models/get-courses.response";
   providedIn: "root"
 })
 export class CoursesService {
+
+  http=inject(HttpClient);
+
+  async loadAllCourses():Promise<Course[]>{
+    const courses$=this.http.get<GetCoursesResponse>(`${environment.apiRoot}/courses`);
+    const response =await firstValueFrom(courses$);// convert obs to promise dhe e kthen ne promise vleren e pare
+    // lastValueFrom(courses$);te fundit port http jan njesoj
+    return response.courses;
+  }
+
+  async createCourse(course:Partial<Course>):Promise<Course>{
+     const course$= this.http.post<Course>(`${environment.apiRoot}/courses`,course)
+     return firstValueFrom(course$)
+    //await mund te neglizohet ketu sepse nuk kemi try catch block qe te kapi errorin
+  }
+
+  async saveCourse(courseId:string,course:Partial<Course>):Promise<Course>{
+    const course$= this.http.put<Course>(`${environment.apiRoot}/courses/${courseId}`,course)
+    return firstValueFrom(course$)
+  }
+
+  async deleteCourse(courseId:string):Promise<any>{
+  const delete$=this.http.delete(`${environment.apiRoot}/courses/${courseId}`)
+     return await firstValueFrom(delete$)
+  }
 
 
 }
